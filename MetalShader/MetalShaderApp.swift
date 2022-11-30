3c6ea74effd64ad5a3d794d1e0a489000f70fb84
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-12-01 07:50:44 +0900

    Initial Commit

---
+++ MetalShader/MetalShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShaderApp.swift
+//  MetalShader
+//
+//  Created by ミズキ on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct MetalShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
